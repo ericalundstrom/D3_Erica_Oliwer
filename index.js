bd0360e82dcd54b13493dbb37f83ad182068bb06
ericalundstrom <erica.lundstrom99@hotmail.com>
2024-02-29 12:53:22 +0100

    added the database

---
+++ index.js
@@ -0,0 +1,4 @@
+(async function () {
+    let data = await d3.csv("healthy_lifestyle_city_2021.csv");
+    console.log(data);
+})()
--- json/index.js
+++
@@ -1,6 +0,0 @@
-async function get_data() {
-  const data = await d3.json("spotify.json");
-  console.log(data);
-}
-
-get_data();
